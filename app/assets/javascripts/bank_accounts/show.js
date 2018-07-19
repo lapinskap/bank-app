5aace67bb4f6099ceaa4b8c54677e406eebed190
Paulina <cherry3475@gmail.com>
2018-07-19 17:31:36 +0200

    add bankAccountId to transaction in show.js

--- app/assets/javascripts/bank_accounts/show.js
+++ app/assets/javascripts/bank_accounts/show.js
@@ -2,8 +2,11 @@ var Show = (function() {
     var $btnNewTransaction;
     var $modalTransaction;
     var $btnSave;
-    var inputAmount;
-    var selectTransactionType;
+    var $inputAmount;
+    var $selectTransactionType;
+    var $parameters;
+
+    var bankAccountId;
 
     var fetchElements = function() {
         $btnNewTransaction = $("#btn-new-transaction");
@@ -11,11 +14,35 @@ var Show = (function() {
         $btnSave = $("#btn-save");
         $inputAmount = $("#input-amount");
         $selectTransactionType = $("#select-transaction-type");
+        $parameters = $("#parameters");
+
+        bankAccountId = $parameters.data("bank-account-id");
+    };
+
+    var disableControls = function() {
+        //disable "save" button after clicking on it
+        $btnSave.prop("disabled",true);
+        $inputAmount.prop("disabled",true);
+        $selectTransactionType.prop("disabled",true);
+    };
+
+    var enableControles = function() {
+        $btnSave.prop("disabled",false);
+        $inputAmount.prop("disabled",false);
+        $selectTransactionType.prop("disabled",false);
     };
 
     var initializeEvents = function() {
         $btnNewTransaction.on("click", function() {
             $modalTransaction.modal("show");
+        });
+        $btnSave.on("click", function () {
+            var amount = $inputAmount.val();
+            var transactionType = $selectTransactionType.val();
+
+            disableControls();
+
+            console.log("Amount: " + amount + " Transaction Type: " + transactionType + " Bank Account Id: " + bankAccountId);
         });
     };
 
