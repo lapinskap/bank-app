e1743d582674cdbd80826078d6565b0ba5adb366
Paulina <cherry3475@gmail.com>
2018-07-19 20:49:52 +0200

    add logic to bank_accounts_controller.rb file

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -5,6 +5,24 @@ module Api
                 amount = params[:amount]
                 transaction_type = params[:transaction_type]
                 bank_account_id = params[:bank_account_id]
+
+                errors = ::BankAccounts::ValidateNewTransaction.new(
+                    amount: amount,
+                    transaction_type: transaction_type,
+                    bank_account_id: bank_account_id
+                ).execute!
+
+                if errors.size > 0
+                    render json: { errors: errors }, status: 402
+                else
+                    bank_account = ::BankAccounts::PerformTransaction.new(
+                        amount: amount,
+                        transaction_type: transaction_type,
+                        bank_account_id: bank_account_id
+                    ).execute!
+                    
+                    render json: { balance: bank_account.balance }
+                end
             end
         end
     end
