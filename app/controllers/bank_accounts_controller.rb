324ab3db42eef00d564b2aa0cb2989d67bdd7750
Paulina <cherry3475@gmail.com>
2018-07-26 17:41:21 +0200

    overwrite new.html.haml file

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -3,9 +3,17 @@ class BankAccountsController < ActionController::Base
         @bank_accounts = BankAccount.all
     end
     def new
+        @bank_account = BankAccount.new
     end
 
     def create
+        @bank_account = BankAccount.new(bank_account_params)
+
+        if @bank_account.save
+            redirect_to bank_account_path(@bank_account.id)
+        else
+            render "new"
+        end
     end
     
     def update
@@ -13,8 +21,14 @@ class BankAccountsController < ActionController::Base
 
     def destroy 
     end
-    
+
     def show
         @bank_account = BankAccount.find(params[:id])
+    end
+
+    private
+
+    def bank_account_params
+        params.require(:bank_account).permit(:account_number, :client_id)
     end
 end
