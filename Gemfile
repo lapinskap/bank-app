ce1786ec29d56164a64f54b155d0524ae65a5b9e
Paulina <cherry3475@gmail.com>
2018-07-26 16:57:01 +0200

    initialize CRUD methods and add simple_form gem

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass'
 gem 'haml'
 gem 'autoprefixer-rails'
+gem 'simple_form'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -2,7 +2,18 @@ class BankAccountsController < ActionController::Base
     def index
         @bank_accounts = BankAccount.all
     end
+    def new
+    end
 
+    def create
+    end
+    
+    def update
+    end
+
+    def destroy 
+    end
+    
     def show
         @bank_account = BankAccount.find(params[:id])
     end
