c415ffcecf0cb1fa502a5f9e75578424294a4c54
Paulina <cherry3475@gmail.com>
2018-07-31 16:51:40 +0200

    add action buttons to new.html.haml and show.html.haml files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_101954) do
+ActiveRecord::Schema.define(version: 2018_07_28_140302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2018_07_18_101954) do
     t.string "middle_name"
     t.string "string"
     t.string "client_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string "author"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
