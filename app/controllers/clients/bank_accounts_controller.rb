96624a19eb8ed4b34507b29c2a277c51ca1085ed
Paulina <cherry3475@gmail.com>
2018-08-02 15:37:16 +0200

    add edit.html.haml file

--- app/controllers/clients/bank_accounts_controller.rb
+++ app/controllers/clients/bank_accounts_controller.rb
@@ -1,11 +1,13 @@
 module Clients
     class BankAccountsController < ApplicationController
+        before_action :load_parent_object
+        before_action :load_object, only: [:edit, :update]
+
         def new
             @bank_account = BankAccount.new
         end
 
         def create
-            @client = Client.find(params[:client_id])
             @bank_account = BankAccount.new(bank_account_params)
             @bank_account.client = @client
 
@@ -17,13 +19,9 @@ module Clients
         end
 
         def edit
-            @client = Client.find(params[:client_id])
-            @bank_account = BankAccount.new(bank_account_params)
         end
 
         def update
-            @client = Client.find(params[:client_id])
-            @bank_account = BankAccount.new(bank_account_params)
             @bank_account.client = @client
 
             if @bank_account.update(bank_account_params)
@@ -34,6 +32,13 @@ module Clients
         end
         
         private
+        def load_object
+            @bank_account = BankAccount.find(params[:id])
+        end
+
+        def load_parent_object
+            @client = Client.find(params[:client_id])
+        end
 
         def bank_account_params
             params.require(:bank_account).permit(:account_number)
